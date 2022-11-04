bbf51202fdee8810209c9fef018d0eb64abb6e12
Brandon Seda <brandon@Brandons-MacBook-Air.local>
2022-11-04 00:20:51 -0400

    added logic to answer questions

--- scripts.js
+++ scripts.js
@@ -1,54 +1,96 @@
 let displayVariable = "";
 let answer = "";
-let answerArray = []
-
-
+let numberArr = []
+let operatorArr = [];
+let numberVar = "";
 
 let textField = document.querySelector(".text-field");
 let numbers = document.getElementsByClassName("number");
 let operators = document.getElementsByClassName("operator");
+let clearBtn = document.querySelector("#clear");
+let deleteBtn = document.querySelector("#delete");
 
+deleteBtn.addEventListener("click", function(){
+    deleteNum();
+});
+
+
+clearBtn.addEventListener("click", function(){
+    clearField();
+});
 
 for(let i = 0; i < numbers.length; i++){
     
     numbers[i].addEventListener("click", function(){
         setDisplay(numbers[i].innerHTML);
+        displayVariable += numbers[i].innerHTML;
+        numberVar += numbers[i].innerHTML;
+        
+
     })
 }
 
+
 for(let i = 0; i < operators.length; i++){
     operators[i].addEventListener("click", function(){
-        setDisplay(operators[i].innerHTML);
+        if(operators[i].innerHTML != "="){
+            setDisplay(operators[i].innerHTML);
+            numberArr.push(numberVar);
+            operatorArr.push(operators[i].innerHTML); 
+            displayVariable += operators[i].innerHTML;
+        } 
+        else {
+            setDisplay(operators[i].innerHTML);
+            if(numberArr.length > 1){
+                numberArr.push(numberVar);
+                operate(operatorArr[0], numberArr[0], numberArr[1]);
+                numberArr.replace(numberArr[1], numberArr[2]);
+                numberArr[0] = answer;
+            }
+        }
+        console.log(numberArr)
+        console.log(operatorArr)
+        numberVar = "";
     })
 }
+
+
+
+
 
 function setDisplay(input){   
     if(input == "+" || input == "-" || input == "/" || input == "*"){
         textField.innerHTML +=  (" " + input + " ");
+    } else if(input == "="){
+        textField.innerHTML = answer;
+        
     } else {
         textField.innerHTML += input;
     }
 }
 
-
-
 function add(a, b){
     answer = a + b
+    return answer;
 }
 
 function subtract(a, b){
     answer = (a - b);
+    return answer;
 }
 
 function multiply(a, b){
     answer = (a * b); 
+    return answer;
 }
 
 function divide(a, b){
     if(b === 0){
-        answer =  "NaN";
+        answer =  "";
+        return answer;
     } else {
         answer = (a / b);
+        return answer;
     }
 }
 
@@ -66,11 +108,25 @@ function operate(operator, a, b){
         default:
             return ""
     }
-
-
 }
 
 
-function clear(){
+function deleteNum(){
+    if(displayVariable.length == 1){
+        displayVariable = ""
+        textField.innerHTML = displayVariable;
+    } 
+    
+        displayVariable = displayVariable.slice(0, displayVariable.length-1);
+        textField.innerHTML = displayVariable;
+    
+}
 
+function clearField(){  
+    textField.innerHTML = "";
+    numberVar = ""
+    answer = "";
+    numberArr = [];
+    operatorArr = [];
+    displayVariable = ""
 }
